656b80c6edc669cde29f513b40256bf3b8fd3c85
ilfey <ilfey.global@gmail.com>
2022-11-16 03:18:20 +0700

    add simple watched earlier list

--- app/src/main/java/com/ilfey/wc/ui/screens/MainScreen.kt
+++ app/src/main/java/com/ilfey/wc/ui/screens/MainScreen.kt
@@ -3,12 +3,17 @@ package com.ilfey.wc.ui.screens
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.rememberPagerState
 import com.ilfey.wc.tabs.TabItem
@@ -17,6 +22,7 @@ import com.ilfey.wc.ui.components.Tabs
 import com.ilfey.wc.ui.components.TabsContent
 import com.ilfey.wc.ui.theme.background
 import com.ilfey.wc.ui.theme.primary
+import com.ilfey.wc.utils.AppConstants
 
 @OptIn(ExperimentalFoundationApi::class, ExperimentalPagerApi::class)
 @Preview
@@ -54,6 +60,39 @@ fun MainScreen() {
             }
         }
 
-//        TODO create watched earlier
+//        TODO add List
+        items(10) {
+            Box {
+                Image(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .heightIn(250.dp)
+                        .align(Alignment.Center),
+                    painter = rememberAsyncImagePainter("${AppConstants.BASEURL}img/600x350.png"), // TODO image from List item
+                    contentDescription = "",
+                )
+
+                Image(
+                    modifier = Modifier
+                        .width(64.dp)
+                        .height(64.dp)
+                        .align(Alignment.Center)
+                        .clickable {
+//                        TODO play movie
+                        },
+                    imageVector = Icons.Filled.PlayArrow,
+                    colorFilter = ColorFilter.tint(primary),
+                    contentDescription = "Play",
+                )
+
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.BottomStart)
+                        .padding(16.dp),
+                    text = "Movie title", // FIXME title from List item
+                    fontSize = 16.sp,
+                )
+            }
+        }
     }
 }
