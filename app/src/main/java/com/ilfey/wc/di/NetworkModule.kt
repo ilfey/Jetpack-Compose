e8d695eb59bced63df900bbbe56b96d90a82e74d
ilfey <ilfey.global@gmail.com>
2022-11-25 15:21:09 +0700

    add `networkModule`

--- app/src/main/java/com/ilfey/wc/di/NetworkModule.kt
+++ app/src/main/java/com/ilfey/wc/di/NetworkModule.kt
@@ -1,1 +1,23 @@
 package com.ilfey.wc.di
+
+import com.ilfey.wc.utils.AppConstants.BASEURL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+val networkModule
+    get() = module {
+        single {
+            val client = OkHttpClient.Builder().apply {
+                addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            }.build()
+
+            Retrofit.Builder().apply {
+                client(client)
+                baseUrl(BASEURL)
+                addConverterFactory(GsonConverterFactory.create())
+            }.build()
+        }
+    }
